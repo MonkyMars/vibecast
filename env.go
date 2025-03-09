947251d5a9160300809d01f3aa37a3d72baf101d
Levi Noppers <levinoppers69@gmail.com>
2025-03-09 21:08:17 +0100

    Replace sensitive API keys with default values in env.go

--- env.go
+++ env.go
@@ -2,9 +2,9 @@ package main
 
 // These values will be overridden at build time
 var (
-	spotifyClientID     = "cfaf06e7acc241cf893bdd897666bb4e"
-	spotifyClientSecret = "62bd8e12d0fe45abbd19b39e3d9f6e4c"
-	weatherAPIKey       = "149948df2606cea6e7c783fa3a9b6f7e"
+	spotifyClientID     = "default"
+	spotifyClientSecret = "default"
+	weatherAPIKey       = "default"
 )
 
 // LoadEnvVars loads environment variables from build flags or returns defaults
